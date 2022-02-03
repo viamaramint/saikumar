69c570e643573535445cb84be5989e2160a7739f
viamaramint <96976163+viamaramint@users.noreply.github.com>
2022-02-03 18:59:03 +0530

    initial

---
+++ sai.js
@@ -0,0 +1,8 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(0,0);
+ctx.lineTo(166,52);
+ctx.stroke();
+ctx.moveTo(166,0);
+ctx.lineTo(0,52);
+ctx.stroke();
